24d6f6ee1fd7bd5cf4389dd912e55e2f5a661047
Rancic <me@rancic.org>
2024-10-23 21:05:47 +0200

    feat: Read metadata and write events to trickfilm

--- spritesheet-bundler/src/main.rs
+++ spritesheet-bundler/src/main.rs
@@ -1,9 +1,14 @@
 use std::collections::HashMap;
-use std::fs::{self, create_dir, read_dir};
+use std::fs::{self, create_dir, read_dir, read_to_string};
 use std::path::Path;
+use std::str::FromStr;
 use std::{env, path::PathBuf};
 
+use anyhow::{anyhow, Result};
 use image::{GenericImage, ImageBuffer, ImageReader};
+
+const RENDER_DIR: &str = "render";
+const METADATA_FILE: &str = "metadata.csv";
 
 const OUTPUT_DIR: &str = "out/";
 const OUTPUT_RON_FILE: &str = "out.trickfilm.ron";
@@ -22,8 +27,219 @@ struct Container {
     animation_data: HashMap<String, u32>,
 }
 
+#[derive(Clone, Copy, Debug)]
+enum AnimationEvents {
+    HitboxStart,
+}
+
+impl ToString for AnimationEvents {
+    fn to_string(&self) -> String {
+        match self {
+            AnimationEvents::HitboxStart => "SpawnHitboxEvent".to_string(),
+        }
+    }
+}
+
+impl FromStr for AnimationEvents {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self> {
+        if s == "hitbox_start" {
+            Ok(Self::HitboxStart)
+        } else {
+            Err(anyhow!("Pose marker is not a valid animation event, '{s}'"))
+        }
+    }
+}
+
+fn animation_parts(path: &PathBuf) -> Vec<String> {
+    path.file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .split('-')
+        .map(|s| s.to_string())
+        .collect()
+}
+
+fn animation_base_name(path: &PathBuf) -> String {
+    animation_parts(path)[0].clone()
+}
+
+fn animation_direction(path: &PathBuf) -> u32 {
+    animation_parts(path)[1]
+        .strip_prefix('o')
+        .unwrap()
+        .parse::<u32>()
+        .unwrap()
+}
+
+fn animation_frame(path: &PathBuf) -> u32 {
+    animation_parts(path)[2]
+        .strip_suffix(".png")
+        .unwrap()
+        .parse::<u32>()
+        .unwrap()
+}
+
+fn animation_name(path: &PathBuf) -> String {
+    let parts: Vec<&str> = path
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .split('-')
+        .collect();
+
+    parts[0].to_string() + "-" + parts[1]
+}
+
+fn animation_format(
+    start_index: u32,
+    end_index: u32,
+    animation: &str,
+    metadata: &HashMap<String, Vec<(AnimationEvents, usize)>>,
+) -> String {
+    let duration = (end_index - start_index) as f32 / FRAME_RATE;
+
+    let mut events_str = String::new();
+    let key = animation
+        .split('-')
+        .next()
+        .expect("Failed to get stem name of animation, unexpected format");
+    if let Some(events) = metadata.get(key) {
+        events_str += "\t\tevents: {\n";
+
+        for (event, frame) in events {
+            events_str += &format!("\t\t\t{}: ", frame);
+            events_str += "{\n";
+            events_str += &format!("\t\t\t\t\"events::{}\": (\n", event.to_string());
+
+            match event {
+                AnimationEvents::HitboxStart => {
+                    events_str += &format!("\t\t\t\t\tmsg: \"{}\",\n", animation);
+                }
+            };
+
+            events_str += "\t\t\t\t)\n";
+            events_str += "\t\t\t},\n";
+        }
+
+        events_str += "\t\t},";
+    }
+
+    format!(
+        "\t\"{animation}\": (
+\t\tkeyframes: KeyframesRange((start: {start_index}, end: {end_index})),
+\t\tduration: {duration},
+{events_str}
+\t),
+"
+    )
+}
+
+fn write_trickfilm_file(images: &Vec<PathBuf>, container: &Container, path: &Path) {
+    let metadata = read_metadata(&path.join(METADATA_FILE));
+    let mut content = String::new();
+    content += "{\n";
+
+    let mut start_index = 0;
+    let mut current_index = 0;
+
+    let mut current_animation = animation_name(&images[0]);
+    let mut current_base_animation = animation_base_name(&images[0]);
+
+    for image in images {
+        let animation_name = animation_name(image);
+        let animation_base_name = animation_base_name(image);
+
+        if animation_name != current_animation {
+            content += &animation_format(start_index, current_index, &current_animation, &metadata);
+            current_animation = animation_name;
+            start_index = animation_direction(image) * container.frames;
+            current_index = start_index;
+        }
+
+        if animation_base_name != current_base_animation {
+            start_index = 0;
+            current_index = 0;
+            current_base_animation = animation_base_name;
+        }
+
+        current_index += 1;
+    }
+    content += &animation_format(start_index, current_index, &current_animation, &metadata);
+
+    content += "}";
+    fs::write(OUTPUT_DIR.to_string() + OUTPUT_RON_FILE, content).unwrap();
+}
+
+fn write_assets_macro(images: &Vec<PathBuf>, container: &Container) {
+    let layout_content = format!(
+        "#[asset(texture_atlas_layout(tile_size_x = {}, tile_size_y = {}, columns = {}, rows = {}))]\npub {}_layout: Handle<TextureAtlasLayout>,\n",
+        container.width,
+        container.height,
+        container.frames,
+        8,
+        NAME_ASSET_MACRO,
+    );
+    let mut animation_content = "\n\n#[asset(paths(\n".to_string();
+
+    let mut current_animation = animation_name(&images[0]);
+    for image in images {
+        let animation_name = animation_name(&image);
+        if animation_name != current_animation {
+            animation_content += &format!(
+                "\"{}#{}\",\n",
+                TRICKFILM_ASSET_MACRO_PATH, current_animation
+            );
+            current_animation = animation_name;
+        }
+    }
+    animation_content += &format!(
+        "\"{}#{}\",\n",
+        TRICKFILM_ASSET_MACRO_PATH, current_animation
+    );
+    animation_content += "),collection(typed))]";
+
+    fs::write(
+        OUTPUT_DIR.to_string() + OUTPUT_ASSET_MACRO_FILE,
+        layout_content + &animation_content,
+    )
+    .unwrap();
+}
+
+fn read_metadata(file: &Path) -> HashMap<String, Vec<(AnimationEvents, usize)>> {
+    let mut hash_map: HashMap<String, Vec<(AnimationEvents, usize)>> = HashMap::new();
+    let contents = read_to_string(file)
+        .expect("Can't open metadata file")
+        .strip_suffix('\n')
+        .expect("Malformed metadata file, supposed to end on a newline")
+        .to_string();
+    for line in contents.split('\n') {
+        let parts: Vec<&str> = line.split(',').collect();
+        let key = parts[0].to_string();
+
+        let mut value = match hash_map.get(&key) {
+            Some(v) => v.to_vec(),
+            None => Vec::new(),
+        };
+
+        value.push((
+            AnimationEvents::from_str(parts[1])
+                .expect("Failed to convert string to AnimationEvent"),
+            parts[2]
+                .parse::<usize>()
+                .expect("Failed to parse string to usize"),
+        ));
+
+        hash_map.insert(key, value);
+    }
+    hash_map
+}
+
 fn main() {
-    let target_path = &env::args().nth(1).expect("Can't find target folder");
+    let path = &env::args().nth(1).expect("Can't find target folder");
+    let target_path = Path::new(path);
 
     if !Path::new(OUTPUT_DIR).exists() {
         create_dir(OUTPUT_DIR).unwrap();
@@ -32,7 +248,7 @@ fn main() {
     let mut container = Container::default();
     let mut images = Vec::new();
 
-    for path in read_dir(target_path).expect("Given path is not correct") {
+    for path in read_dir(target_path.join(RENDER_DIR)).expect("Given path is not correct") {
         let path = match path {
             Ok(r) => r,
             Err(err) => panic!("Something wrong with path, {}", err),
@@ -98,129 +314,6 @@ fn main() {
             .unwrap();
     }
 
-    write_trickfilm_file(&images, &container);
+    write_trickfilm_file(&images, &container, target_path);
     write_assets_macro(&images, &container);
-}
-
-fn animation_parts(path: &PathBuf) -> Vec<String> {
-    path.file_name()
-        .unwrap()
-        .to_str()
-        .unwrap()
-        .split('-')
-        .map(|s| s.to_string())
-        .collect()
-}
-
-fn animation_base_name(path: &PathBuf) -> String {
-    animation_parts(path)[0].clone()
-}
-
-fn animation_direction(path: &PathBuf) -> u32 {
-    animation_parts(path)[1]
-        .strip_prefix('o')
-        .unwrap()
-        .parse::<u32>()
-        .unwrap()
-}
-
-fn animation_frame(path: &PathBuf) -> u32 {
-    animation_parts(path)[2]
-        .strip_suffix(".png")
-        .unwrap()
-        .parse::<u32>()
-        .unwrap()
-}
-
-fn animation_name(path: &PathBuf) -> String {
-    let parts: Vec<&str> = path
-        .file_name()
-        .unwrap()
-        .to_str()
-        .unwrap()
-        .split('-')
-        .collect();
-
-    parts[0].to_string() + "-" + parts[1]
-}
-
-fn animation_format(start_index: u32, end_index: u32, animation: &str) -> String {
-    let duration = (end_index - start_index) as f32 / FRAME_RATE;
-    format!(
-        "\t\"{animation}\": (
-\t\tkeyframes: KeyframesRange((start: {start_index}, end: {end_index})),
-\t\tduration: {duration},
-\t),
-"
-    )
-}
-
-fn write_trickfilm_file(images: &Vec<PathBuf>, container: &Container) {
-    let mut content = String::new();
-    content += "{\n";
-
-    let mut start_index = 0;
-    let mut current_index = 0;
-
-    let mut current_animation = animation_name(&images[0]);
-    let mut current_base_animation = animation_base_name(&images[0]);
-
-    for image in images {
-        let animation_name = animation_name(image);
-        let animation_base_name = animation_base_name(image);
-
-        if animation_name != current_animation {
-            content += &animation_format(start_index, current_index, &current_animation);
-            current_animation = animation_name;
-            start_index = animation_direction(image) * container.frames;
-            current_index = start_index;
-        }
-
-        if animation_base_name != current_base_animation {
-            start_index = 0;
-            current_index = 0;
-            current_base_animation = animation_base_name;
-        }
-
-        current_index += 1;
-    }
-    content += &animation_format(start_index, current_index, &current_animation);
-
-    content += "}";
-    fs::write(OUTPUT_DIR.to_string() + OUTPUT_RON_FILE, content).unwrap();
-}
-
-fn write_assets_macro(images: &Vec<PathBuf>, container: &Container) {
-    let layout_content = format!(
-        "#[asset(texture_atlas_layout(tile_size_x = {}, tile_size_y = {}, columns = {}, rows = {}))]\npub {}_layout: Handle<TextureAtlasLayout>,\n",
-        container.width,
-        container.height,
-        container.frames,
-        8,
-        NAME_ASSET_MACRO,
-    );
-    let mut animation_content = "\n\n#[asset(paths(\n".to_string();
-
-    let mut current_animation = animation_name(&images[0]);
-    for image in images {
-        let animation_name = animation_name(&image);
-        if animation_name != current_animation {
-            animation_content += &format!(
-                "\"{}#{}\",\n",
-                TRICKFILM_ASSET_MACRO_PATH, current_animation
-            );
-            current_animation = animation_name;
-        }
-    }
-    animation_content += &format!(
-        "\"{}#{}\",\n",
-        TRICKFILM_ASSET_MACRO_PATH, current_animation
-    );
-    animation_content += "),collection(typed))]";
-
-    fs::write(
-        OUTPUT_DIR.to_string() + OUTPUT_ASSET_MACRO_FILE,
-        layout_content + &animation_content,
-    )
-    .unwrap();
 }
